a95306f42ab35153c2475bb85d033a67da176b02
Joey Faas <joeyfaas@gmail.com>
2018-09-22 08:15:06 -0400

    Added Lab03

---
+++ Lab03/app/src/main/java/jjf25/cs262/calvin/edu/LoginId/SecondActivity.java
@@ -0,0 +1,34 @@
+package jjf25.cs262.calvin.edu.LoginId;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "jjf25.cs262.calvin.edu.LoginId.extra.REPLY";
+    private EditText mReply;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mReply = (EditText) findViewById(R.id.editText_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textview = (TextView) findViewById(R.id.text_message);
+        textview.setText(message);
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
